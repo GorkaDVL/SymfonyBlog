74f2cc54a5515162108191555db66159630b8479
Gorka D <gorka.devergara-laporte@laposte.net>
2021-10-01 11:06:59 +0200

    controler-template changes

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,16 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo)
     {
+
+        $articles = $repo->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -29,9 +35,12 @@ class BlogController extends AbstractController
         ]);
     }
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      */
-    public function show(){
-        return $this->render('blog/show.html.twig');
+    public function show(Article $article){
+    
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
